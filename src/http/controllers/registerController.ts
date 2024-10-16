24552c374e7a3061a2a8208908cb3c955f1c21e5
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-10-16 18:07:20 -0300

    Repository pattern

--- src/http/controllers/registerController.ts
+++ src/http/controllers/registerController.ts
@@ -1,6 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { prisma } from "@/lib/prisma";
 import { z } from "zod";
+import { registerService } from "@/services/registerService";
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
   const registerbodySchema = z.object({
@@ -11,13 +11,15 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerbodySchema.parse(req.body);
 
-  await prisma.user.create({
-    data: {
+  try {
+    await registerService({
       name,
       email,
-      password_hash: password,
-    },
-  });
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send();
+  }
 
   return reply.status(201).send();
 }
---
+++ src/services/registerService.ts
@@ -0,0 +1,31 @@
+import { PrismaUsersRepository } from './../repositories/prisma-users-repository';
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+
+interface RegisterServiceRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerService({ name, email, password}: RegisterServiceRequest) {
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    }
+  });
+
+  if (userWithSameEmail) {
+    throw new Error("E-mail already exists.");
+  }
+
+  const prismaUsersRepository = new PrismaUsersRepository
+
+  prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  });
+}
