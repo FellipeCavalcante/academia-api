939cefbaa2a7224111f4e339245856dd7f28ddac
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-10-21 20:35:55 -0300

    handler global errors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { prisma } from "./lib/prisma";
-import { register } from "./http/controllers/registerController";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _req, replay) => {
+  if (error instanceof ZodError) {
+    return replay.status(400).send({ message: "Validation error.", issus: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+      console.error(error);
+  } else {
+    //TODO: 
+  }
+
+  return replay.status(500).send("Internal server error");
+})
--- src/http/controllers/registerController.ts
+++ src/http/controllers/registerController.ts
@@ -3,6 +3,7 @@ import { PrismaUsersRepository } from './../../repositories/prisma/prisma-users-
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 import { RegisterService } from "@/services/registerService";
+import { UserAlreadyExistsError } from '@/services/errors/user-already-exists';
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
   const registerbodySchema = z.object({
@@ -23,7 +24,11 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (error) {
-    return reply.status(409).send();
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message});
+    }
+
+    throw error;
   }
 
   return reply.status(201).send();
