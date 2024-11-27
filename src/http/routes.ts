2b47582299150533551e3603c409ce986036a096
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-11-27 17:36:26 -0300

    perfil controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from "fastify";
 import { register } from "./controllers/registerController";
 import { autheticate } from "./controllers/authenticateController";
 import { profile } from "./controllers/profileController";
+import { verifyJWT } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", autheticate);
 
   /** Authenticated */
-  app.get("/me", profile); 
+  app.get("/me", { onRequest: [verifyJWT] } ,profile); 
 }
