2d4d860b5aaa34b4f0ce6f02c54d358898b1a33d
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-11-28 17:39:46 -0300

    check-ins e2e tests

--- src/http/controllers/check-ins/historyController.ts
+++ src/http/controllers/check-ins/historyController.ts
@@ -16,7 +16,7 @@ export async function history(req: FastifyRequest, reply: FastifyReply) {
     page,
   });
 
-  return reply.status(201).send({
+  return reply.status(200).send({
     checkIns,
   });
 }
