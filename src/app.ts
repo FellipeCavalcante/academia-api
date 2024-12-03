2dc8b9656f582793e57408f9ae2078f739fd1c5b
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-12-03 15:47:27 -0300

    refresh token

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
 import { checkInsRoutes } from "./http/controllers/check-ins/routes";
@@ -10,22 +11,33 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
-})
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
-app.register(checkInsRoutes)
+app.register(checkInsRoutes);
 
 app.setErrorHandler((error, _req, replay) => {
   if (error instanceof ZodError) {
-    return replay.status(400).send({ message: "Validation error.", issus: error.format() });
+    return replay
+      .status(400)
+      .send({ message: "Validation error.", issus: error.format() });
   }
 
   if (env.NODE_ENV !== "production") {
-      console.error(error);
+    console.error(error);
   } else {
-    //TODO: 
+    //TODO:
   }
 
   return replay.status(500).send("Internal server error");
-})
+});
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,12 +3,14 @@ import { register } from "./registerController";
 import { autheticate } from "./authenticateController";
 import { profile } from "./profileController";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
-
+import { refresh } from "./refreshController";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", autheticate);
 
+  app.patch("/token/refresh", refresh);
+
   /** Authenticated */
-  app.get("/me", { onRequest: [verifyJWT] } ,profile); 
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
